dfbdb34d92741473222640c9cba285434ca6361e
just-mn <77879381+just-mn@users.noreply.github.com>
2023-04-28 02:09:37 +0300

    rename
    less cringe now

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# LastFMYM
+# YandexMusic Scrobbler
 ![](https://img.shields.io/github/license/vilander1337/WeatherMod?color=B34AEE)
 <br>
 ### A simple script for ppls which wanna back lastfm to yandex music integration
 ## How to install
-<code>sh -c "$(curl -fsSL https://raw.githubusercontent.com/just-mn/lastfmym/main/setup.sh)" && python lastfmym.py</code>
+<code>sh -c "$(curl -fsSL https://raw.githubusercontent.com/just-mn/YandexMusicScrobbler/main/setup.sh)" && python lastfmym.py</code>
 ### How to get a yandex music token
-#### Follow the [link](https://oauth.yandex.com/authorize?response_type=token&client_id=23cabbbdc6cd418abb4b39c32c41195d), log into your yandex account, copy the access_token value from the address bar
+#### Follow the [link](https://oauth.yandex.com/authorize?response_type=token&client_id=23cabbbdc6cd418abb4b39c32c41195d), log into your Yandex account and after refreshing the page, copy the access_token value from the address bar as soon as possible before you are transferred to the web player
--- setup.sh
+++ setup.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 clear
 echo "
-
-  _      _   ___ _____ ___ __  __  __   ____  __ 
- | |    /_\ / __|_   _| __|  \/  | \ \ / /  \/  |
- | |__ / _ \\__ \ | | | _|| |\/| |  \ V /| |\/| |
- |____/_/ \_\___/ |_| |_| |_|  |_|   |_| |_|  |_|
-                                                 
+ __     __             _           __  __           _         _____                _     _     _           
+ \ \   / /            | |         |  \/  |         (_)       / ____|              | |   | |   | |          
+  \ \_/ /_ _ _ __   __| | _____  _| \  / |_   _ ___ _  ___  | (___   ___ _ __ ___ | |__ | |__ | | ___ _ __ 
+   \   / _` | '_ \ / _` |/ _ \ \/ / |\/| | | | / __| |/ __|  \___ \ / __| '__/ _ \| '_ \| '_ \| |/ _ \ '__|
+    | | (_| | | | | (_| |  __/>  <| |  | | |_| \__ \ | (__   ____) | (__| | | (_) | |_) | |_) | |  __/ |   
+    |_|\__,_|_| |_|\__,_|\___/_/\_\_|  |_|\__,_|___/_|\___| |_____/ \___|_|  \___/|_.__/|_.__/|_|\___|_|   
+                                                                                                           
 "
-git clone https://github.com/just-mn/lastfmym
+git clone https://github.com/just-mn/YandexMusicScrobbler
 cd lastfmym
 pip install -r requirements.txt
 clear
 echo "
-
-  _      _   ___ _____ ___ __  __  __   ____  __ 
- | |    /_\ / __|_   _| __|  \/  | \ \ / /  \/  |
- | |__ / _ \\__ \ | | | _|| |\/| |  \ V /| |\/| |
- |____/_/ \_\___/ |_| |_| |_|  |_|   |_| |_|  |_|
-                                                 
+ __     __             _           __  __           _         _____                _     _     _           
+ \ \   / /            | |         |  \/  |         (_)       / ____|              | |   | |   | |          
+  \ \_/ /_ _ _ __   __| | _____  _| \  / |_   _ ___ _  ___  | (___   ___ _ __ ___ | |__ | |__ | | ___ _ __ 
+   \   / _` | '_ \ / _` |/ _ \ \/ / |\/| | | | / __| |/ __|  \___ \ / __| '__/ _ \| '_ \| '_ \| |/ _ \ '__|
+    | | (_| | | | | (_| |  __/>  <| |  | | |_| \__ \ | (__   ____) | (__| | | (_) | |_) | |_) | |  __/ |   
+    |_|\__,_|_| |_|\__,_|\___/_/\_\_|  |_|\__,_|___/_|\___| |_____/ \___|_|  \___/|_.__/|_.__/|_|\___|_|   
+                                                                                                           
 "
 
 read -p "Enter LastFM username: " lastfmusername
 read -p "Enter LastFM password: " lastfmpasswd
 read -p "Enter yandex music token (check how to get token in readme.md): " ymtoken
 
-sed -i 's/LastFM_username = "UR_LASTFM_USER"/LastFM_username = '"'$lastfmusername'"'/' lastfmym.py
-sed -i 's/LastFM_password = "UR_LASTFM_PASSWD"/LastFM_password = '"'$lastfmpasswd'"'/' lastfmym.py
-sed -i 's/YandexMusic_TOKEN = "UR_YM_TOKEN"/YandexMusic_TOKEN = '"'$ymtoken'"'/' lastfmym.py
+sed -i 's/LastFM_username = "UR_LASTFM_USER"/LastFM_username = '"'$lastfmusername'"'/' scrobbler.py
+sed -i 's/LastFM_password = "UR_LASTFM_PASSWD"/LastFM_password = '"'$lastfmpasswd'"'/' scrobbler.py
+sed -i 's/YandexMusic_TOKEN = "UR_YM_TOKEN"/YandexMusic_TOKEN = '"'$ymtoken'"'/' scrobbler.py

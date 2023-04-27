5f959c1b63152bb68252339d8f346a52a00d3312
vilander1337 <117035823+vilander1337@users.noreply.github.com>
2023-04-28 01:33:05 +0400

    fix main script for install.sh

--- lastfmym.py
+++ lastfmym.py
@@ -6,10 +6,10 @@ import time
 API_KEY = "6b5d8a7decab0c7b547a90dbbf37efc7"
 API_SECRET = "9778621ef705328e52b161bd76a7ac40"
 
-LastFM_username = "" # <- Your LastFM username
-LastFM_password = "" # <- Your LastFM password
+LastFM_username = "UR_LASTFM_USER" # <- Your LastFM username
+LastFM_password = "UR_LASTFM_PASSWD" # <- Your LastFM password
 
-YandexMusic_TOKEN = "" # <- Your YandexMusic token ()
+YandexMusic_TOKEN = "UR_YM_TOKEN" # <- Your YandexMusic token
 
 network = pylast.LastFMNetwork(
     api_key=API_KEY,
